f357bbade5560e2ccdb53b9f241719b055b26c30
1000ch <shogo.sensui@gmail.com>
2015-03-16 23:57:13 +0900

    build static resources

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,34 @@ var concat   = require('gulp-concat');
 var sequence = require('run-sequence').use(gulp);
 
 gulp.task('default', function () {
-  sequence('js', 'css')
+  sequence('js', 'css');
 });
 
 gulp.task('js', function () {
+  gulp.start('js:lib', 'js:app');
+});
+
+gulp.task('css', function () {
+  gulp.start('css:lib', 'css:app');
+});
+
+gulp.task('js:lib', function () {
+
+  var files = [
+    'bower_components/jquery/dist/jquery.min.js'
+  ];
+
+  gulp.src(files)
+    .pipe(concat('lib.min.js'))
+    .pipe(gulp.dest('_public/js'));
+});
+
+gulp.task('js:app', function () {
 
   var uglify = require('gulp-uglify');
 
   var files = [
-    'static/js/ga.js',
-    'static/js/script.js'
+    'static/js/app.js'
   ];
 
   gulp.src(files)
@@ -21,16 +39,30 @@ gulp.task('js', function () {
     .pipe(gulp.dest('_public/js'));
 });
 
-gulp.task('css', function () {
+gulp.task('css:lib', function () {
 
   var csscomb = require('gulp-csscomb');
   var csso    = require('gulp-csso');
 
   var files = [
     'bower_components/normalize.css/normalize.css',
-    'bower_components/highlight.js/src/styles/solarized_light.css',
-    'static/css/default.css',
-    'static/css/style.css'
+    'bower_components/highlight.js/src/styles/solarized_light.css'
+  ];
+
+  gulp.src(files)
+    .pipe(concat('lib.min.css'))
+    .pipe(csscomb())
+    .pipe(csso())
+    .pipe(gulp.dest('_public/css'));
+});
+
+gulp.task('css:app', function () {
+
+  var csscomb = require('gulp-csscomb');
+  var csso    = require('gulp-csso');
+
+  var files = [
+    'static/css/app.css'
   ];
 
   gulp.src(files)
