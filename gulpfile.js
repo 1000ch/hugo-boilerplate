9c5bd088f118e990552880e5435e5a3ba5f1d8a6
1000ch <shogo.sensui@gmail.com>
2015-03-17 00:04:03 +0900

    gulp.watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,35 @@ var gulp     = require('gulp');
 var concat   = require('gulp-concat');
 var sequence = require('run-sequence').use(gulp);
 
-gulp.task('default', function () {
+var LIB_JS_FILES = [
+  'bower_components/jquery/dist/jquery.min.js'
+];
+
+var APP_JS_FILES = [
+  'static/js/app.js'
+];
+
+var LIB_CSS_FILES = [
+  'bower_components/normalize.css/normalize.css',
+  'bower_components/highlight.js/src/styles/solarized_light.css'
+];
+
+var APP_CSS_FILES = [
+  'static/css/app.css'
+];
+
+gulp.task('watch', function () {
+
+  gulp.watch(APP_JS_FILES, function () {
+    gulp.start('js:app');
+  });
+
+  gulp.watch(APP_CSS_FILES, function () {
+    gulp.start('css:app');
+  });
+});
+
+gulp.task('build', function () {
   sequence('js', 'css');
 });
 
@@ -16,11 +44,7 @@ gulp.task('css', function () {
 
 gulp.task('js:lib', function () {
 
-  var files = [
-    'bower_components/jquery/dist/jquery.min.js'
-  ];
-
-  gulp.src(files)
+  gulp.src(LIB_JS_FILES)
     .pipe(concat('lib.min.js'))
     .pipe(gulp.dest('_public/js'));
 });
@@ -29,11 +53,7 @@ gulp.task('js:app', function () {
 
   var uglify = require('gulp-uglify');
 
-  var files = [
-    'static/js/app.js'
-  ];
-
-  gulp.src(files)
+  gulp.src(APP_JS_FILES)
     .pipe(concat('app.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('_public/js'));
@@ -44,12 +64,7 @@ gulp.task('css:lib', function () {
   var csscomb = require('gulp-csscomb');
   var csso    = require('gulp-csso');
 
-  var files = [
-    'bower_components/normalize.css/normalize.css',
-    'bower_components/highlight.js/src/styles/solarized_light.css'
-  ];
-
-  gulp.src(files)
+  gulp.src(LIB_CSS_FILES)
     .pipe(concat('lib.min.css'))
     .pipe(csscomb())
     .pipe(csso())
@@ -61,11 +76,7 @@ gulp.task('css:app', function () {
   var csscomb = require('gulp-csscomb');
   var csso    = require('gulp-csso');
 
-  var files = [
-    'static/css/app.css'
-  ];
-
-  gulp.src(files)
+  gulp.src(APP_CSS_FILES)
     .pipe(concat('app.min.css'))
     .pipe(csscomb())
     .pipe(csso())
